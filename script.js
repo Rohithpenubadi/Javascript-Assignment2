a760f47c4bdd862076e1a1f531542d32a95ac0e8
Rohith <97825827+Rohithpenubadi@users.noreply.github.com>
2022-07-26 19:07:33 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+function display(val){
+    document.getElementById('result').value += val
+    return val
+}
+
+function solve(){
+
+    let x = document.getElementById('result').value
+
+    let y = eval(x);
+    if(y % 1 != 0) y = y.toFixed(5)
+    if(y == Infinity) y = "Cannot be divided by zero"
+
+    document.getElementById('result').value = y
+
+    return y
+
+}
+
+function backSpace() {
+    let x = document.getElementById('result').value
+    let y = x.toString().slice(0, -1)
+    document.getElementById('result').value = y
+    return y
+}
+
+function clearScreen(){
+
+    document.getElementById('result').value = ''
+
+}
