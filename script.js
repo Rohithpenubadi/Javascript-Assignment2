b4f49635c73ed29244072a6abe354dba9a3db0e5
Rohith <97825827+Rohithpenubadi@users.noreply.github.com>
2022-07-27 22:38:16 +0530

    second commit

--- script.js
+++ script.js
@@ -1,30 +1,80 @@
-function display(val){
-    document.getElementById('result').value += val
-    solve()
-    return val
+let queue = [];
+let input = 0;
+function solve(value) {
+    if (input !== 0) {
+        input = parseFloat(input);
+
+        addToQueue(input);
+    }
+    let result = value[0];
+    let dividedByZero = 0;
+    for (let i = 2; i < value.length; i = i + 2) {
+
+        switch (queue[i - 1]) {
+            case '+':
+                result += value[i];
+                break;
+            case '-':
+                result -= value[i];
+                break;
+            case '/': if (value[i] === 0) dividedByZero = 1;
+            else result = result / value[i];
+
+                break;
+            case '*': result = result * value[i];
+                break;
+        }
+
+    }
+
+    result = result.toFixed(6);
+    result = parseFloat(result);
+    if (dividedByZero === 1) {
+        clearScreen();
+        document.getElementById("result").value = "Cannot divide by zero";
+    }
+    else {
+        document.getElementById("result").value = result;
+        input = result;
+        queue = [];
+    }
 }
-
-function solve(){
-
-    let x = document.getElementById('result').value
-
-    let y = eval(x);
-    if(y % 1 != 0) y = y.toFixed(5)
-    if(y == Infinity) y = "Cannot be divided by zero"
-
-    document.getElementById('result').value = y
-
-    return y
-
+function addToQueue(input) {
+    queue.push(input);
 }
-
 function backSpace() {
     let x = document.getElementById('result').value
     let y = x.toString().slice(0, -1)
     document.getElementById('result').value = y
     return y
 }
-
-function clearScreen(){
-    document.getElementById('result').value = ''
+function clearScreen() {
+    queue = [];
+    input = 0;
+    document.getElementById("result").value = "0";
+}
+function display(item) {
+    if (
+        document.getElementById("result").value === "Cannot divide by zero" ||
+        (document.getElementById("result").value == "0" && item != ".")
+    ) {
+        document.getElementById("result").value = "";
+    }
+    if (!(item === ".") || !input.match(/[.]/)) {
+        input += item;
+        document.getElementById("result").value += item;
+    }
+}
+function operator(item) {
+    if (input !== 0 && input !== "-") {
+        input = parseFloat(input);
+        addToQueue(input);
+        addToQueue(item);
+        document.getElementById("result").value += item;
+        input = 0;
+    }
+    if (item == "-" && isNaN(queue[0]) && input !== "-") {
+        input = "-";
+        document.getElementById("result").value = "-";
+    }
 }
